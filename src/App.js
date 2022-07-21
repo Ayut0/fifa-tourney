94922f6da958a242c7e12e978119e85afbd31c1f
Yuto Yamakita <yuvto.0420@gmail.com>
2022-07-20 17:03:26 -0700

    Displayed match result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import MatchesList from './components/MatchesList';
 import PlayersList from './components/PlayersList';
+import { Box } from '@mui/material';
+import Container from "@mui/material/Container";
 
 function App() {
   return (
@@ -8,7 +10,21 @@ function App() {
       <h1>Fifa Tourney</h1>
       <main>
         <PlayersList />
-        <MatchesList />
+        <Container maxWidth="xl">
+          <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                  m: 1,
+                  width: .32,
+                  height: 230,
+                },
+              }}
+            >
+            <MatchesList />
+          </Box>
+        </Container>
       </main>
     </div>
   );
--- src/components/Match.js
+++ src/components/Match.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Box from "@mui/material/Box";
+import { Paper } from '@mui/material';
 
-export default function Match() {
-  return (<div>
-    <h2>a</h2>
-  </div>);
+export default function Match(props) {
+  console.log(props.players.id1);
+  const playersArray2 = props.players;
+  console.log(playersArray2)
+  const winner = (props.match.winner === 0 ? 'None of them': `${props.players[`id${props.match.winner}`].name}`)
+  return (
+    <Paper elevation={3}>
+      <h2>Match No, {props.match.id}</h2>
+      <h2>{`Player ${
+        props.players[`id${props.match.players[0]}`].name
+      } against Player ${
+        props.players[`id${props.match.players[1]}`].name
+      }`}</h2>
+      <h2>Score Diff, {props.match.scoreDifference}</h2>
+      <h2>Winner, {winner}</h2>
+    </Paper>
+  );
 }
+ 
